a2844a1e08b68b69b25441b48b23e654732c03b5
tengler <t.engler@hotmail.de>
2018-02-18 12:29:45 +0100

    Problem 22 solved, problem 26 started.

---
+++ euler/src/main/java/org/euler/common/Strings.java
@@ -0,0 +1,8 @@
+package org.euler.common;
+
+public class Strings {
+	public static String removeStringCharacter(String pString, String pRemoveString) {
+		return pString.replace(pRemoveString, "");
+	}
+
+}
